cd570336dd6e09563be67c29cbba5bf3c7eab6ab
abenetsol <solabenet@gmail.com>
2023-01-23 20:38:15 +0000

    task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject(report) {
   const emps = [];
   // for loop
-	for (const dep of Object.keys(report.allEmployees)) {
+  for (const dep of Object.keys(report.allEmployees)) {
     // nested for loop
-		for (const emp of report.allEmployees[dep]) {
+    for (const emp of report.allEmployees[dep]) {
       emps.push(emp);
     }
   }
