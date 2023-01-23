41ab9c7f33d0fa0b081ea9232e9f1e746ab2cf09
abenetsol <solabenet@gmail.com>
2023-01-23 20:28:15 +0000

    task adv

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,12 @@
+export default function createIteratorObject(report) {
+  const emps = [];
+  // for loop
+	for (const dep of Object.keys(report.allEmployees)) {
+    // nested for loop
+		for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
+    }
+  }
+
+  return emps;
+}
